4129d354ab1dbfa989ccf8aa6756418b85a3e523
nimatamizi <159938938+nimatamizi@users.noreply.github.com>
2024-03-22 11:16:19 +0000

    initial
    release

---
+++ scan.py
@@ -0,0 +1,52 @@
+import socket
+import threading
+from queue import Queue
+import sys
+import time
+
+
+lock = threading.Lock()
+
+ip = input("IP:")
+
+queue = Queue()
+
+def portscan(port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        out = s.connect_ex((target.port))
+        if out == 0:
+            with lock:
+                    print("port open :", port)
+        s.close()
+    except:
+        pass
+
+def worker():
+    while True:
+        port= queue.get()
+        portscan(port)
+        queue.task_done()
+threads = 100
+
+for thread in range(thread_count):
+    thread = threading.Thread(ip=worker)
+    thread.daemon = True
+    thread.start()
+
+start = int(input("Start port:"))
+end = int(input("End port:"))
+
+for port in range(start, end + 1):
+    queue.put(port)
+
+try:
+    for _ in range(start, end + 1):
+        sys.stdout.flush()
+        time.sleep(0.05) #thats for the loading bar adjustment
+        sys.stdout.write('\rScanning ports: [{0:50s}] {1:.1f}%'.format('#' * int((_ - start + 1) * 50 / (end - start + 1)), (_ - start + 1) * 100 / (end - start + 1))) #loading bar
+finally:
+    queue.join()
+
+print('scanning is done.')
